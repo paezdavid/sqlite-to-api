a383c77456a3d9c80433219112eaea2bb9fd1c20
paezdavid <davidgithub1@gmail.com>
2023-04-15 17:17:12 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+### Clonar el repositorio
+- git clone https://github.com/paezdavid/sqlite-to-api.git
+
+# En Linux:
+
+### Dentro de la carpeta abrir una terminal y ejecutar:
+- pip install -r requirements.txt
+
+### Inicializar el servidor de FastAPI
+- python -m uvicorn main:app --reload
--- db_example.py
+++ db_example.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, json
 
 # Hay que crear una conexión a la base de datos desde este documento Python
 conn = sqlite3.connect("example.db")
@@ -7,8 +7,7 @@ conn = sqlite3.connect("example.db")
 cursor = conn.cursor()
 
 # Le pasamos los comandos a execute() para que el cursor los ejecute.
-# Acá creamos una tabla:
-
+# Acá creamos una tabla (solo ejecutar una vez, la primera vez):
 # cursor.execute('''
 #     CREATE TABLE users
 #     (id INTEGER PRIMARY KEY,
@@ -18,21 +17,26 @@ cursor = conn.cursor()
 
 # Se pueden hacer tantos execute() como sean necesarios y van a irse añadiendo a la DB.
 # Con INSERT, insertamos registros a nuestra DB:
-
 cursor.execute('''
     INSERT INTO users (name, age) VALUES (?, ?)
 ''', ('Sebastian', 11))
 
-cursor.execute('''
-    INSERT INTO users (name, age) VALUES (?, ?)
-''', ('Tobias', 14))
+# cursor.execute('''
+#     INSERT INTO users (name, age) VALUES (?, ?)
+# ''', ('Tobias', 14))
 
 # Podemos hacer SELECT y guardar los registros en una variable
 cursor.execute("SELECT * FROM users")
 registros = cursor.fetchall()
 
+# Lista de tuples
+print(registros)
+
+# El [0] es la primera fila, el [1] es la segunda columna (name)
+print(registros[0][1])
+
 for fila in registros:
-    print(fila)
+    print(fila) # Se imprime cada fila
 
 conn.commit() # Se committean los cambios a la DB
 conn.close() # Se cierra la conexión una vez realizados y committeados los cambios
